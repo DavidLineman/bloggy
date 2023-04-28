c6c4955ab740e485c1784bbee96c781b53d06613
DavidLineman <dline215@gmail.com>
2023-04-28 13:57:33 -0400

    added more model tests

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -2,7 +2,26 @@ require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
   test "draft? returns true for draft blog post" do
-    binding.irb
-    BlogPost.new(published_at: nil).draft?
+    assert BlogPost.new(published_at: nil).draft?
   end
+
+  test "draft? returns false for published blog post" do
+    refute BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute BlogPost.new(published_at: 1.year.from_now).draft?
+  end
+
+  test "published? returns true for published blog post" do 
+
+  end 
+
+  test "published? returns false for draft blog post" do 
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published returns false fro scheduled blog post" do 
+
+  end 
 end
